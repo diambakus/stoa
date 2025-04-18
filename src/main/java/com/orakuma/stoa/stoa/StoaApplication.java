527853eccd0c30dd8c3512bbf9873f23aa92771d
diambakus <lazoss@gmail.com>
2025-04-18 21:41:43 +0200

    first commit

---
+++ src/main/java/com/orakuma/stoa/stoa/StoaApplication.java
@@ -0,0 +1,13 @@
+package com.orakuma.stoa.stoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoaApplication.class, args);
+	}
+
+}
