0ea3b1c78ae801376f5a66f8a328b01dbec1a97a
Adroaldo Lazouriano Moreira Borges <lazoss@gmail.com>
2025-10-03 15:06:12 +0200

    deploy on render set up.

--- src/main/java/com/orakuma/stoa/auth/AuthenticationController.java
+++ src/main/java/com/orakuma/stoa/auth/AuthenticationController.java
@@ -1,7 +1,7 @@
 package com.orakuma.stoa.auth;
 
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+//import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+//import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -9,9 +9,9 @@ import reactor.core.publisher.Mono;
 @RestController
 public class AuthenticationController {
 
-    @GetMapping(value = "/token")
+    /*@GetMapping(value = "/token")
     public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient("keycloak") OAuth2AuthorizedClient authorizedClient) {
         String freshToken = authorizedClient.getAccessToken().getTokenValue();
         return Mono.just(freshToken);
-    }
+    }*/
 }
--- src/main/java/com/orakuma/stoa/config/security/SecurityConfig.java
+++ src/main/java/com/orakuma/stoa/config/security/SecurityConfig.java
@@ -1,15 +1,17 @@
-package com.orakuma.stoa.config;
+package com.orakuma.stoa.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.context.annotation.Profile;
+/*import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;*/
 
-import static org.springframework.security.config.Customizer.withDefaults;
+//import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
+/*@Configuration
 @EnableWebFluxSecurity
+@Profile("!local")
 public class SecurityConfig {
 
     @Bean
@@ -25,4 +27,4 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .build();
     }
-}
+}*/
